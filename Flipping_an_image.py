ee6f5ef54d31f320359260a58cb338b4646f878e
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-09-06 16:03:22 +0900

    Create Flipping_an_image.py

---
+++ Flipping_an_image.py
@@ -0,0 +1,14 @@
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        result_matrix =[]
+        for row in A:
+            row.reverse()
+            result = map(lambda x: 1 if x==0 else 0, row)
+            result_matrix.append(result)
+    
+        return result_matrix
+        
+'''
+Runtime: 48 ms	
+Memory: 13.9 MB
+'''
