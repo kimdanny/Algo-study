16a3cfdeeb9fb1445ecbef3f1beabeef7f0790c3
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-11-22 02:05:10 +0000

    Create counting_valleys.py

---
+++ counting_valleys.py
@@ -0,0 +1,28 @@
+"
+HackerRank.
+Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+For example, if Gary's path is [DDUUUUDD], he first enters a valley 2 units deep. 
+Then he climbs out an up onto a mountain 2 units high. Finally, he returns to sea level and ends his hike.
+"
+def countingValleys(n, s):
+    numOfValleys = 0
+    level = 0
+    valStart = True
+
+    for x in s:
+        if x=='U':
+            level+=1
+            if level<0 and valStart:
+                numOfValleys+=1
+                valStart = False
+            if level >= 0:
+                valStart = True
+        else:
+            level -= 1
+            if level<0 and valStart:
+                numOfValleys+=1
+                valStart = False
+            if level >= 0:
+                valStart = True
+
+    return numOfValleys
