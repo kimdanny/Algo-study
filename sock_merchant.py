a7d1bd1bd147ebe1cb1d4461a17273310f0fe1f9
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-11-22 02:16:30 +0000

    Create sock_merchant.py

---
+++ sock_merchant.py
@@ -0,0 +1,18 @@
+"""
+John works at a clothing store. 
+He has a large pile of socks that he must pair by color for sale.
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+For example, there are n=7 socks with colors ar=[1, 2, 1, 2, 1, 3, 2]. There is one pair of color 1 and one of color 2. 
+There are three odd socks left, one of each color. The number of pairs is 2.
+"""
+
+def sockMerchant(n, ar):
+    record = Counter(ar)
+    count = 0
+    for num in dict(record).values():
+        if num%2==0:
+            count+=num/2
+        else:
+            count+= (num-1)/2
+    
+    return int(count)
